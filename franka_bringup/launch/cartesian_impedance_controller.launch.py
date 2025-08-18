d87602f27d17ff831eb721a3cd072efcf2ebb463
Aly Ahmed <alynader2004@gmail.com>
2025-08-18 16:12:12 -0400

    Launch file and launch configuration for cartesian controller:
    Added a new yaml file which includes custom configuration
    for the cartesian impedance controller. Also added a launch file
    specific to the cartesian impedance controller which allows you
    to launch the cartesian impedance controller directly for testing

---
+++ franka_bringup/launch/cartesian_impedance_controller.launch.py
@@ -0,0 +1,214 @@
+#  Copyright (c) 2021 Franka Emika GmbH
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription,
+                            Shutdown)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+import yaml
+
+ 
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:  # parent of IOError, OSError *and* WindowsError where available
+        return None
+
+
+def generate_launch_description():
+    robot_ip_parameter_name = 'robot_ip'
+    load_gripper_parameter_name = 'load_gripper'
+    use_fake_hardware_parameter_name = 'use_fake_hardware'
+    fake_sensor_commands_parameter_name = 'fake_sensor_commands'
+    use_rviz_parameter_name = 'use_rviz'
+
+    robot_ip = LaunchConfiguration(robot_ip_parameter_name)
+    load_gripper = LaunchConfiguration(load_gripper_parameter_name)
+    use_fake_hardware = LaunchConfiguration(use_fake_hardware_parameter_name)
+    fake_sensor_commands = LaunchConfiguration(fake_sensor_commands_parameter_name)
+    use_rviz = LaunchConfiguration(use_rviz_parameter_name)
+
+    # Command-line arguments
+
+    db_arg = DeclareLaunchArgument(
+        'db', default_value='False', description='Database flag'
+    )
+
+    # planning_context
+    franka_xacro_file = os.path.join(get_package_share_directory('franka_description'), 'robots',
+                                     'panda_arm.urdf.xacro')
+    robot_description_config = Command(
+        [FindExecutable(name='xacro'), ' ', franka_xacro_file, ' hand:=true',
+         ' robot_ip:=', robot_ip, ' use_fake_hardware:=', use_fake_hardware,
+         ' fake_sensor_commands:=', fake_sensor_commands])
+
+    robot_description = {'robot_description': robot_description_config}
+
+    franka_semantic_xacro_file = os.path.join(get_package_share_directory('franka_moveit_config'),
+                                              'srdf',
+                                              'panda_arm.srdf.xacro')
+    robot_description_semantic_config = Command(
+        [FindExecutable(name='xacro'), ' ', franka_semantic_xacro_file, ' hand:=true']
+    )
+    robot_description_semantic = {
+        'robot_description_semantic': robot_description_semantic_config
+    }
+
+    # RViz
+   
+    rviz_file = os.path.join(get_package_share_directory('franka_description'), 'rviz',
+                             'visualize_franka.rviz')
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='log',
+        arguments=['-d', rviz_file],
+        parameters=[
+            robot_description,
+            robot_description_semantic,
+            # ompl_planning_pipeline_config,
+            # kinematics_yaml,
+        ],
+        condition=IfCondition(use_rviz)
+    )
+
+    # Publish TF
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[robot_description],
+    )
+
+    ros2_controllers_fake_path = os.path.join(
+        get_package_share_directory('franka_bringup'),
+        'config',
+        'panda_ros_controllers_fake.yaml'
+    )
+
+    ros2_controllers_real_path = os.path.join(
+        get_package_share_directory('franka_bringup'),
+        'config',
+        'cartesian_impedance_ros_controllers_real.yaml'
+    )
+
+    ros2_control_node_fake = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, ros2_controllers_fake_path],
+        remappings=[('joint_states', 'franka/joint_states')],
+        output={'stdout': 'screen', 'stderr': 'screen'},
+        condition=IfCondition(use_fake_hardware),
+        on_exit=Shutdown()
+    )
+
+    ros2_control_node_real = Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[robot_description, ros2_controllers_real_path],
+        remappings=[('joint_states', 'franka/joint_states')],
+        output={'stdout': 'screen', 'stderr': 'screen'},
+        condition=UnlessCondition(use_fake_hardware),
+        on_exit=Shutdown()
+    )
+
+    # Load controllers
+    load_controllers = []
+    for controller in ['cartesian_impedance_controller', 'joint_state_broadcaster']:
+        load_controllers += [
+            ExecuteProcess(
+                cmd=['ros2 run controller_manager spawner {}'.format(controller)],
+                shell=True,
+                output='screen',
+            )
+        ]
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[
+            {'source_list': ['franka/joint_states', 'panda_gripper/joint_states'], 'rate': 30}],
+    )
+
+    franka_robot_state_broadcaster = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['franka_robot_state_broadcaster'],
+            output='screen',
+            condition=UnlessCondition(use_fake_hardware),
+    )
+
+    robot_arg = DeclareLaunchArgument(
+        robot_ip_parameter_name,
+        description='Hostname or IP address of the robot.')
+    use_rviz_arg = DeclareLaunchArgument(
+            use_rviz_parameter_name,
+            default_value='true',
+            description='Visualize the robot in Rviz')
+    use_fake_hardware_arg = DeclareLaunchArgument(
+        use_fake_hardware_parameter_name,
+        default_value='false',
+        description='Use fake hardware')
+    fake_sensor_commands_arg = DeclareLaunchArgument(
+        fake_sensor_commands_parameter_name,
+        default_value='false',
+        description="Fake sensor commands. Only valid when '{}' is true".format(
+            use_fake_hardware_parameter_name))
+    gripper_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([PathJoinSubstitution(
+            [FindPackageShare('franka_gripper'), 'launch', 'gripper.launch.py'])]),
+        launch_arguments={'robot_ip': robot_ip,
+                          use_fake_hardware_parameter_name: use_fake_hardware}.items(),
+    )
+
+    cartesian_impedance_node = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['cartesian_impedance_controller'],
+        output='screen',
+    )
+
+    return LaunchDescription(
+        [robot_arg,
+         use_rviz_arg,
+         use_fake_hardware_arg,
+         fake_sensor_commands_arg,
+         db_arg,
+         rviz_node,
+         robot_state_publisher,
+         ros2_control_node_fake,
+         ros2_control_node_real,
+         joint_state_publisher,
+         franka_robot_state_broadcaster,
+         gripper_launch_file,
+         cartesian_impedance_node
+         ]
+        + load_controllers
+    )
